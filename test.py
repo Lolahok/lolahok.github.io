f832dadb05d25c5929395f4ee4b8c4192d9700e8
Lolahok <lolataylor2410@gmail.com>
2024-06-24 21:50:44 +0330

    Update test.py

--- test.py
+++ test.py
@@ -18,13 +18,28 @@ def main():
     
     found_urls = []  # List to store URLs where keyword was found
     
-    headers = {
+    header = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
+    }
+    
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    
+    proxies = {
+        "http": "http://127.0.0.1:8080",
+        "https": "http://127.0.0.1:8080"
     }
     
     payloads = [
         "nexiz%22", "nexiz%2522", 'nexiz%27', "nexiz%2527",
         "nexiz%3C", "nexiz%253C"
+    ]
+    
+    pays = [
+        'nexiz%22', "nexiz%25%32%32", "nexiz%27", "nexiz%25%32%37",
+        "nexiz%3C", "nexiz%25%33%63"
     ]
     
     keywords = ["nexiz'", 'nexiz"', "nexiz<"]
@@ -41,7 +56,7 @@ def main():
             # Check GET requests with payloads
             for payload in progress_bar:
                 url = f"{base_url}&{param}={payload}"
-                response = requests.get(url, headers=headers)
+                response = requests.get(url, headers=header)
                 
                 if any(keyword in response.text for keyword in keywords):
                     found_any = True
@@ -52,11 +67,11 @@ def main():
             
             # Additional checks with 'test' and 'nexiz' parameters
             test_url = f"{base_url}&{param}=test&{param}=nexiz"
-            response = requests.get(test_url, headers=headers)
+            response = requests.get(test_url, headers=header)
             if "nexiz" in response.text:
                 for payload in payloads:
                     url = f"{base_url}&{param}=test&{param}={payload}"
-                    response = requests.get(url, headers=headers)
+                    response = requests.get(url, headers=header)
                     
                     if any(keyword in response.text for keyword in keywords):
                         found_any = True
@@ -66,17 +81,31 @@ def main():
                     time.sleep(1)
             
             # Check POST requests with payloads
-            for payload in payloads:
+            for payload in pays:
                 data = {param: payload}
                 response = requests.post(base_url, headers=headers, data=data)
-                
+                print(f"Payload '{data}' sent. Response status code: {response.status_code}")
                 if any(keyword in response.text for keyword in keywords):
-                    found_any = Trueprox
+                    found_any = True
                     found_urls.append(f"POST: {data}")
                     progress_bar.set_postfix(found="YES", refresh=True)
                     progress_bar.set_description(f"Found {param}", refresh=True)
                 time.sleep(1)
-            
+                
+            datas = {param: ['test', 'nexiz']}
+            response = requests.post(base_url, headers=headers, data=datas)
+            print(f"Payload '{datas}' sent. Response status code: {response.status_code}")
+            if "nexiz" in response.text:
+                for payload in pays:
+                    dataz = {param: ['test', payload]}
+                    response = requests.post(base_url, headers=headers, data=dataz)
+                    if any(keyword in response.text for keyword in keywords):
+                        found_any = True
+                        found_urls.append(f"POST: {dataz}")
+                        progress_bar.set_postfix(found="YES", refresh=True)
+                        progress_bar.set_description(f"Found {param}", refresh=True)
+                    time.sleep(1)
+                
             if found_any:
                 print(f"Found: {param}")
             else:
